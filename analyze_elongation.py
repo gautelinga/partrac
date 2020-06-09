a786eac230f8c7acb82fa478f971f5fcb02054fc
Gaute Linga <gaute.linga@gmail.com>
2020-06-09 14:49:35 +0200

    More features to analyze elongation.

--- analyze_elongation.py
+++ analyze_elongation.py
@@ -14,6 +14,8 @@ parser.add_argument("--show", action="store_true", help="Show")
 parser.add_argument("--terminal", action="store_true", help="Print to terminal")
 parser.add_argument("-o", "--output", type=str, default="", help="Output")
 parser.add_argument("-nstd", type=int, default=5, help="Number of stds to include in data")
+parser.add_argument("--all", action="store_true", help="Do it on all")
+parser.add_argument("--nolog", action="store_true", help="No logarithm")
 args = parser.parse_args()
 
 params = Params(args.folder)
@@ -32,43 +34,53 @@ possible_fields = [["u", "Vector", "Node"],
                    ["dl", "Scalar", "Edge"],
                    ["dl0", "Scalar", "Edge"]]
 
-it = np.argmin(abs(np.array(ts)-args.t))
+if not args.all:
+    it = np.argmin(abs(np.array(ts)-args.t))
+    t = ts[it]
+    print("Found", t)
+    ts = [t]
 
-t = ts[it]
+filename = os.path.join(args.folder, "elongdata.dat")
+elongdatafile = open(filename, "w")
 
-print("Found", t)
+print("{}\t{}\t{}\t{}".format("t", "mean", "var", "std"))
+for t in ts:
+    posft, grp = posf[t]
+    fields = []
+    with h5py.File(posft, "r") as h5f:
+        if grp + "/dA" in h5f and grp + "/dA0" in h5f:
+            if not args.all:
+                print("Found face areas")
+            dA = np.array(h5f[grp + "/dA"])[:, 0]
+            dA0 = np.array(h5f[grp + "/dA0"])[:, 0]
+            elong = dA/dA0
+            w = dA0
+        elif grp + "/dl" in h5f and grp + "/dl0" in h5f:
+            if not args.all:
+                print("Found edge lengths")
+            dl = np.array(h5f[grp + "/dl"])[:, 0]
+            dl0 = np.array(h5f[grp + "/dl0"])[:, 0]
+            elong = dl/dl0
+            w = dl0
+        else:
+            print("Does not contain this.")
+            exit()
 
-posft, grp = posf[t]
-fields = []
-with h5py.File(posft, "r") as h5f:
-    if grp + "/dA" in h5f and grp + "/dA0" in h5f:
-        print("Found face areas")
-        dA = np.array(h5f[grp + "/dA"])[:, 0]
-        dA0 = np.array(h5f[grp + "/dA0"])[:, 0]
-        elong = dA/dA0
-        w = dA0
-    elif grp + "/dl" in h5f and grp + "/dl0" in h5f:
-        print("Found edge lengths")
-        dl = np.array(h5f[grp + "/dl"])[:, 0]
-        dl0 = np.array(h5f[grp + "/dl0"])[:, 0]
-        elong = dl/dl0
-        w = dl0
+    if args.nolog:
+        data = elong
     else:
-        print("Does not contain this.")
-        exit()
-
-logrho = np.log(elong)
-logrho_mean = np.mean(logrho*w)/np.mean(w)
-logrho_var = np.mean((logrho-logrho_mean)**2*w)/np.mean(w)
-logrho_std = np.sqrt(logrho_var)
-print("logrho_mean =", logrho_mean)
-print("logrho_var  =", logrho_var)
-print("logrho_std  =", logrho_std)
+        data = np.log(elong)
+    data_mean = np.mean(data*w)/np.mean(w)
+    data_var = np.mean((data-data_mean)**2*w)/np.mean(w)
+    data_std = np.sqrt(data_var)
+    string = "{}\t{}\t{}\t{}".format(t, data_mean, data_var, data_std)
+    print(string)
+    elongdatafile.write(string + "\n")
 
 hist, bin_edges = np.histogram(np.log(elong), weights=w, density=True,
                                bins=args.bins,
-                               range=(logrho_mean-args.nstd*logrho_std,
-                                      logrho_mean+args.nstd*logrho_std))
+                               range=(data_mean-args.nstd*data_std,
+                                      data_mean+args.nstd*data_std))
 x = 0.5*(bin_edges[1:]+bin_edges[:-1])
 
 if args.show:
@@ -83,4 +95,3 @@ if args.output != "":
     with open(args.output, "w") as ofile:
         for xi, hi in zip(x, hist):
             ofile.write("{}\t{}\n".format(xi, hi))
-
